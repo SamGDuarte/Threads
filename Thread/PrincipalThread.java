570876cf6e85264db2fb3eb1d34c3c466ac857be
SamGDuarte <126623537+SamGDuarte@users.noreply.github.com>
2023-03-21 19:59:53 -0300

    Add files via upload

---
+++ Thread/PrincipalThread.java
@@ -0,0 +1,9 @@
+package HTML.Faculdade.Thread;
+
+public class PrincipalThread {
+    public static void main(String [] args) {
+
+        System.out.println("Numero de threads: " + Thread.activeCount());
+        
+    }
+}
